fa5958ed8b499a286f8f07a11caf1ea3fa9f3b84
miyagi-h-tech <mhkawsdd1027@gmail.com>
2025-09-04 18:05:12 +0900

    プロジェクト全体にテーマ(thema)を適用する

---
+++ src/theme/theme.ts
@@ -0,0 +1,9 @@
+import { createTheme } from "@mui/material";
+
+export const theme = createTheme({
+    typography: {
+        fontFamily: 'Noto Sans JP, Roboto, "Helvetica Neue", Arial, sans-serif',
+        fontWeightRegular: 400, // フォントウェイト400
+        fontWeightMedium: 500, // フォントウェイト500
+        fontWeightBold: 700, // フォントウェイト700
+    })
