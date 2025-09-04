8838a84366eb6dfdde1895bcd8ebdc01f36be5ca
miyagi-h-tech <mhkawsdd1027@gmail.com>
2025-09-04 16:09:58 +0900

    Outletを使用したRoute、Material UIインストール

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,17 @@ import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Report from './pages/Report';
 import NoMatch from './pages/NoMatch';
+import AppLayout from './components/layout/AppLayout';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="/report" element={<Report />}></Route>
-        <Route path="*" element={<NoMatch />}></Route>
+        <Route path="/" element={<AppLayout />}>
+          <Route index element={<Home />}></Route>
+          <Route path="/report" element={<Report />}></Route>
+          <Route path="*" element={<NoMatch />}></Route>
+        </Route>
       </Routes>
     </Router>
   );
