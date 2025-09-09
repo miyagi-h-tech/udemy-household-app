206c9d17028fdb9878396c2a34470a9ac0de56c5
miyagi-h-tech <mhkawsdd1027@gmail.com>
2025-09-09 13:59:18 +0900

    日付ごとの収支を計算する関数を作成

--- src/components/Calender.tsx
+++ src/components/Calender.tsx
@@ -4,13 +4,22 @@ import dayGridPlugin from '@fullcalendar/daygrid';
 import jaLocal from '@fullcalendar/core/locales/ja';
 import "../calender.css"
 import { EventContentArg } from '@fullcalendar/core';
+import { Transaction } from '../types';
+import { calculateDailyBlances } from '../utils/financeCalculations';
 
-function Calender() {
+interface ClanderProps {
+  monthlyTransactions: Transaction[]
+}
+
+function Calender({ monthlyTransactions }: ClanderProps) {
   const events = [
-  { title: 'Meeting', start: new Date() }
+    { title: 'Meeting', start: new Date() }
   ]
 
-  const renderEventContent =(eventInfo: EventContentArg) => {
+  const dailyBalances = calculateDailyBlances(monthlyTransactions);
+  console.log(dailyBalances);
+
+  const renderEventContent = (eventInfo: EventContentArg) => {
     return (
       <div>
         <div className="money" id="event-income">
