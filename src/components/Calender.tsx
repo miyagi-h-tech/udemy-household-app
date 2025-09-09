33651c6f47fe307344b3af4cb32ae40c92529cf3
miyagi-h-tech <mhkawsdd1027@gmail.com>
2025-09-09 13:46:17 +0900

    fullカレンダーライブラリのインストール

--- src/components/Calender.tsx
+++ src/components/Calender.tsx
@@ -1,8 +1,39 @@
+import FullCalendar from '@fullcalendar/react'
 import React from 'react'
+import dayGridPlugin from '@fullcalendar/daygrid';
+import jaLocal from '@fullcalendar/core/locales/ja';
+import "../calender.css"
+import { EventContentArg } from '@fullcalendar/core';
 
 function Calender() {
+  const events = [
+  { title: 'Meeting', start: new Date() }
+  ]
+
+  const renderEventContent =(eventInfo: EventContentArg) => {
+    return (
+      <div>
+        <div className="money" id="event-income">
+          {eventInfo.event.extendedProps.income}
+        </div>
+        <div className="money" id="event-expense">
+          {eventInfo.event.extendedProps.income}
+        </div>
+        <div className="money" id="event-balance">
+          {eventInfo.event.extendedProps.income}
+        </div>
+      </div>
+    )
+  }
+
   return (
-    <div>Calender</div>
+    <FullCalendar
+      locale={jaLocal}
+      plugins={[dayGridPlugin]}
+      initialView='dayGridMonth'
+      events={events}
+      eventContent={renderEventContent}
+    />
   )
 }
 
