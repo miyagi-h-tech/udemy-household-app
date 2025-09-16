f32c3e524c1ee5dbd115e9eca8052cc6619eacdb
miyagi-h-tech <mhkawsdd1027@gmail.com>
2025-09-16 15:35:28 +0900

    Report画面の大まかなレイアウト

--- src/pages/Report.tsx
+++ src/pages/Report.tsx
@@ -1,8 +1,35 @@
+import { Grid, Paper } from '@mui/material'
 import React from 'react'
+import MonthSelector from '../components/MonthSelector'
+import CategoryChart from '../components/CategoryChart'
+import BarChart from '../components/BarChart'
+import TransactionTable from '../components/TransactionTable'
 
 const Report = () => {
+  const commonPaperStyle = {
+    height: { xs: "auto", md: "400px" },
+    display: "flex",
+    flexDerection: "column",
+  }
   return (
-    <div>Report</div>
+    <Grid container spacing={2}>
+      <Grid size={{ xs: 12 }}>
+        <MonthSelector />
+      </Grid>
+      <Grid size={{ xs: 12, md: 4 }}>
+        <Paper sx={commonPaperStyle}>
+          <CategoryChart />
+        </Paper>
+      </Grid>
+      <Grid size={{ xs: 12, md: 8 }}>
+        <Paper sx={commonPaperStyle}>
+          <BarChart />
+        </Paper>
+      </Grid>
+      <Grid size={{ xs: 12 }}>
+        <TransactionTable />
+      </Grid>
+    </Grid>
   )
 }
 
