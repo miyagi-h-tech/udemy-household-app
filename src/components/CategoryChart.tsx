3b5222e250802ab48c898694a14e763dca4bc13c
miyagi-h-tech <mhkawsdd1027@gmail.com>
2025-09-17 16:26:54 +0900

    収支タイプ・カテゴリごとの合計金額を計算、円グラフにデータを反映

--- src/components/CategoryChart.tsx
+++ src/components/CategoryChart.tsx
@@ -1,45 +1,68 @@
 import React, { useState } from 'react'
-import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
+import { Chart as ChartJS, ArcElement, Tooltip, Legend, ChartData } from 'chart.js';
 import { Pie } from 'react-chartjs-2';
-import { Box, FormControl, InputLabel, MenuItem, Select, SelectChangeEvent, TextField } from '@mui/material';
-import { TransactionType } from '../types';
+import { Box, CircularProgress, FormControl, InputLabel, MenuItem, Select, SelectChangeEvent, TextField, Typography } from '@mui/material';
+import { ExpenseCategory, IncomeCategory, Transaction, TransactionType } from '../types';
 
 ChartJS.register(ArcElement, Tooltip, Legend);
 
+interface CategoryChartProps {
+  monthlyTransactions: Transaction[];
+  isLoading: boolean;
+}
 
-const data = {
-  labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-  datasets: [
-    {
-      label: '# of Votes',
-      data: [12, 19, 3, 5, 2, 3],
-      backgroundColor: [
-        'rgba(255, 99, 132, 0.2)',
-        'rgba(54, 162, 235, 0.2)',
-        'rgba(255, 206, 86, 0.2)',
-        'rgba(75, 192, 192, 0.2)',
-        'rgba(153, 102, 255, 0.2)',
-        'rgba(255, 159, 64, 0.2)',
-      ],
-      borderColor: [
-        'rgba(255, 99, 132, 1)',
-        'rgba(54, 162, 235, 1)',
-        'rgba(255, 206, 86, 1)',
-        'rgba(75, 192, 192, 1)',
-        'rgba(153, 102, 255, 1)',
-        'rgba(255, 159, 64, 1)',
-      ],
-      borderWidth: 1,
-    },
-  ],
-};
-
-function CategoryChart() {
+function CategoryChart({ monthlyTransactions, isLoading }: CategoryChartProps) {
   const [selectedType, setSelectedType] = useState<TransactionType>("expense");
 
   const handleTypeChange = (e: SelectChangeEvent<"income" | "expense">) => {
     setSelectedType(e.target.value as TransactionType);
   }
+
+  const categorySums =
+    monthlyTransactions.filter(
+      (transaction) => transaction.type === selectedType
+    ).reduce<Record<IncomeCategory | ExpenseCategory, number>>((acc, transaction) => {
+      if (!acc[transaction.category]) {
+        acc[transaction.category] = 0;
+      }
+      acc[transaction.category] += transaction.amount;
+      return acc;
+    }, {} as Record<IncomeCategory | ExpenseCategory, number>
+    );
+
+  const categoryLabels = Object.keys(categorySums);
+  const categoryValues = Object.values(categorySums);
+
+  const options = {
+    maintainAspectRatio: false,
+    responsive: true,
+  };
+
+  const data: ChartData<"pie"> = {
+    labels: categoryLabels,
+    datasets: [
+      {
+        data: categoryValues,
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)',
+        ],
+        borderColor: [
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)',
+        ],
+        borderWidth: 1,
+      },
+    ],
+  };
 
   return (
     <Box>
@@ -52,18 +75,32 @@ function CategoryChart() {
 
       <FormControl fullWidth>
         <InputLabel id="type-select-label">収支の種類</InputLabel>
-        <Select 
-        labelId="type-select-label" 
-        id="type-select" 
-        value={selectedType}
-        label="収支の種類" 
-        onChange={handleTypeChange}
-      >
-        <MenuItem value={"income"}>収入</MenuItem>
-        <MenuItem value={"expense"}>支出</MenuItem>
-      </Select>
+        <Select
+          labelId="type-select-label"
+          id="type-select"
+          value={selectedType}
+          label="収支の種類"
+          onChange={handleTypeChange}
+        >
+          <MenuItem value={"income"}>収入</MenuItem>
+          <MenuItem value={"expense"}>支出</MenuItem>
+        </Select>
       </FormControl>
-      <Pie data={data} />
+      <Box sx={{
+        flexGrow: 1,
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}>
+
+        {isLoading ? (
+          <CircularProgress />
+        ) : monthlyTransactions.length > 0 ? (
+          <Pie data={data} options={options} />
+        ) : (
+          <Typography>データがありません</Typography>
+        )}
+      </Box>
     </Box>
   )
 }
